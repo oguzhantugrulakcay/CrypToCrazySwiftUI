3e9b26d571275e821b8e2e1c1bb2a315349f185f
oguzhantugrulakcay <oguzhantugrulakcay@hotmail.com>
2024-06-16 00:58:38 +0300

    Initial Commit

---
+++ CrypToCrazySwiftUI/CrypToCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypToCrazySwiftUIApp.swift
+//  CrypToCrazySwiftUI
+//
+//  Created by Oğuzhantuğrul Akçay on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CrypToCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
